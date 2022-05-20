ff1896799406ab0a86416ad57cfc8f2080f88d88
apatrida <jayson.minard@gmail.com>
2022-05-20 03:27:04 -0600

    remove unused integration test module

--- integration-tests/src/test/java/dev/sort/oss/quarkus/opensearch/client/it/OpensearchClientResourceIT.java
+++
@@ -1,7 +0,0 @@
-package dev.sort.oss.quarkus.opensearch.client.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class OpensearchClientResourceIT extends OpensearchClientResourceTest {
-}
