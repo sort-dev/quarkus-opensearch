96f3bee78936e2f35a04436c818c3ee41c375433
Jayson Minard <jayson.minard@gmail.com>
2022-12-27 12:24:36 -0600

    change container version to match OpenSearch version dependency, to 2.4.1 from 2.3.0

--- deployment/src/main/java/dev/sort/oss/quarkus/opensearch/client/deployment/OpenSearchDevServicesBuildTimeConfig.java
+++ deployment/src/main/java/dev/sort/oss/quarkus/opensearch/client/deployment/OpenSearchDevServicesBuildTimeConfig.java
@@ -29,7 +29,7 @@ public class OpenSearchDevServicesBuildTimeConfig {
      * The container image to use.
      * Defaults to the OpenSearch image provided by the OpenSearch project.
      */
-    @ConfigItem(defaultValue = "opensearchproject/opensearch:2.3.0")
+    @ConfigItem(defaultValue = "opensearchproject/opensearch:2.4.1")
     public String imageName;
 
     /**
